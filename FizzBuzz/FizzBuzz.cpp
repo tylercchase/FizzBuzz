e3c5a2647f2ca4fd136fb8a553ba2839ee5420a1
Jigglebox <tylercchase@gmail.com>
2019-01-09 13:12:56 -0900

    content

--- FizzBuzz/FizzBuzz.cpp
+++ FizzBuzz/FizzBuzz.cpp
@@ -3,19 +3,27 @@
 
 #include "pch.h"
 #include <iostream>
-
+void fizz(int input);
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	for (int c = 0; c <= 100; c++) {
+		fizz(c);
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void fizz(int input) {
+	bool isBuzzable = false;
+	if (input % 3 == 0) {
+		std::cout << "fizz";
+		isBuzzable = true;
+	}
+	if (input % 5 == 0) {
+		std::cout << "buzz";
+		isBuzzable = true;
+	}
+	if (isBuzzable) {
+		std::cout << std::endl;
+	}
+	else {
+		std::cout << input << std::endl;
+	}
+}
